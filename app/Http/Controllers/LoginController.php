ae7e89c77e842a48e8104ff8cc143db9100b2d2d
tintb03 <128471718+tintb03@users.noreply.github.com>
2023-07-28 18:47:06 +0700

    login phân quyền

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,17 +16,30 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
+        $credentials = $request->validate([
+            'role' => 'required|in:user,admin', // Role phải là 'user' hoặc 'admin'
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
-        $role = $request->input('role');
-        $guard = $role === 'admin' ? 'admin' : 'web';
+        $loginType = filter_var($request->input('email'), FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        $request->merge([$loginType => $request->input('email')]);
 
-        if (Auth::guard($guard)->attempt($credentials)) {
-            // Đăng nhập thành công
-            return redirect()->route($role === 'admin' ? 'admin.index' : 'home');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->role === 'admin') {
+                // Đăng nhập thành công và role là admin
+                return redirect()->route('dashboard')->with('success', 'You have successfully logged in as admin.');
+            } else {
+                // Đăng nhập thành công và role là user
+                return redirect()->route('home')->with('success', 'You have successfully logged in as user.');
+            }
         } else {
             // Đăng nhập thất bại
-            return redirect()->route('login')->with('error', 'Invalid credentials');
+            return redirect()->back()->withErrors([
+                'message' => 'Incorrect account or password.',
+            ]);
         }
     }
 }
+
