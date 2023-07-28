72c38978217d4ba3e10363f861ecac9a0a17eb35
tintb03 <tinpdbh00128@fpt.edu.vn>
2023-07-28 12:28:17 +0700

    route home login register

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('HomePage.main');
 });
+
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+
+// Trang đăng ký
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+// Trang người dùng
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+// Trang admin
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+    // Thêm các route khác cho trang admin nếu cần
+});
+
